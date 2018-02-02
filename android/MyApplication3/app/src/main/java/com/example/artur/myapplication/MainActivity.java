32fd841ead78215eaf2b97d00a75b2a0842c14ee
Arti Kondziela <artur.kondziela@gmail.com>
2018-02-02 20:43:02 +0100

    First working version of Android app

--- android/MyApplication3/app/src/main/java/com/example/artur/myapplication/MainActivity.java
+++ android/MyApplication3/app/src/main/java/com/example/artur/myapplication/MainActivity.java
@@ -1,24 +1,114 @@
 package com.example.artur.myapplication;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final String REQUEST_URL = "http://localhost:8000/user/";
+
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtView = (TextView) findViewById(R.id.txtContent);
+
         final Button scanButton = findViewById(R.id.bt_scan);
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Dizala", Toast.LENGTH_SHORT);
+//                Toast.makeText(getApplicationContext(), "Dizala", Toast.LENGTH_LONG).show();
+                dispatchTakePictureIntent();
+
             }
         });
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            Barcode barcode = this.parseQRCode(imageBitmap);
+            sendDataToServer(barcode.rawValue);
+        }
+    }
+
+    private Barcode parseQRCode(Bitmap bitmap) {
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){
+            txtView.setText("Could not set up the detector!");
+            return null;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+        // TODO[AKO]: text field to check if code is correct -> to delete after implementation
+        txtView.setText(thisCode.rawValue);
+        return thisCode;
+    }
+
+    private void sendDataToServer(String dataToSend) {
+        URL url;
+        try {
+            url = new URL(REQUEST_URL);
+            String body = "qrcode=" + dataToSend;
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(URLEncoder.encode(body, "UTF-8"));
+
+            writer.flush();
+            writer.close();
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
